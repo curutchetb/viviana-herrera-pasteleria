a699438071f1bbab848a049b9a5d8c90bc909fb0
curutchetb <77202352+curutchetb@users.noreply.github.com>
2026-04-14 18:34:43 -0300

    Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import sitemap from '@astrojs/sitemap'; <--- COMENTÁ ESTA LÍNEA
 
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://viviana-herrera-pasteleria.vercel.app',
-  integrations: [
-    tailwind(),
-    // sitemap(), <--- COMENTÁ ESTA LÍNEA TAMBIÉN
-  ],
 });
