5f28be5ac77d6f8ce6e89761e1f644cf8bde9c4d
BCurutchet [SBI] <barbara.curutchet@sbi.suhaiseguradora.com>
2026-04-14 12:35:11 -0300

    refactor: disable sitemap to fix build and clear cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap'; <--- COMENTÁ ESTA LÍNEA
 
 export default defineConfig({
   site: 'https://viviana-herrera-pasteleria.vercel.app',
   integrations: [
     tailwind(),
-    sitemap(),
+    // sitemap(), <--- COMENTÁ ESTA LÍNEA TAMBIÉN
   ],
 });
